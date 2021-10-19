574c70e6348d6438bf852670672f5b4e71a6c9c4
sebastiangolijow <you@example.com>
2021-10-19 20:06:08 +0200

    upgrade

--- src/calculateTeamPoints.js
+++ src/calculateTeamPoints.js
@@ -4,67 +4,37 @@
 // input: [{ team: 'green', name: 'Bob', points: 5, isActive: true }, ...]
 // output: [{ team: 'green', points: 40 }, ...]
 
-const players = [
-  {
-      "team": "red",
-      "name": "john",
-      "points": 10,
-      "isActive": false
-  },
-  {
-      "team": "red",
-      "name": "mike",
-      "points": 20,
-      "isActive": true
-  },
-  {
-      "team": "blue",
-      "name": "mike",
-      "points": 10,
-      "isActive": true
-  },
-  {
-      "team": "green",
-      "name": "carl",
-      "points": 15,
-      "isActive": true
-  },
-  {
-      "team": "green",
-      "name": "jose",
-      "points": 25,
-      "isActive": true
-  },
-  {
-      "team": "red",
-      "name": "sue",
-      "isActive": false,
-      "points": 35
-  }
-]
+const players = require('./__tests__/__data__/players.json')
 
+function eliminarObjetosDuplicados(arr, prop) {
+  var nuevoArray = [];
+  var lookup  = {};
 
-const calculateTeamPoints = (players) => {
-  let result = [{team:'red', points:0}, {team:'green', points:0}, {team:'blue', points:0}, ];
-  for (var i = 0; i < result.length; i++){
-    players.map(item => {
-      if(item.isActive === true){
-        if(item.team === result[i].team){
-          result[i].points += item.points
-        }
-      }
-    })
+  for (var i in arr) {
+      lookup[arr[i][prop]] = arr[i];
   }
-  return result.sort(function (a, b) {
-    if (a.points < b.points) {
-      return 1;
-    }
-    if (a.points > b.points) {
-      return -1;
-    }
-    return 0;
-  });
+
+  for (i in lookup) {
+      nuevoArray.push(lookup[i]);
+  }
+
+  return nuevoArray;
 }
 
+const calculateTeamPoints = (players) => {
+  let result = [];
+    players.map(item => {
+      if(item.isActive === true){
+        result.push({team: item.team, points: 0})
+        result.map(value => {
+          if(item.team === value.team){
+            value.points += item.points
+          }
+        })
+      }
+    })
+  return  eliminarObjetosDuplicados(result, 'team');
+}; 
 
 console.log(calculateTeamPoints(players));
+
