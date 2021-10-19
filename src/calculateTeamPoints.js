cd9cf30b4ac8b9debdc5dc2f3f9552f9bf32acdd
sebastiangolijow <you@example.com>
2021-10-19 22:23:43 +0200

    upgrade check teams complete

--- src/calculateTeamPoints.js
+++ src/calculateTeamPoints.js
@@ -7,45 +7,31 @@
 const players = require('./__tests__/__data__/players.json')
 
 const calculateTeamPoints = (players) => {
-  let result = [];
-  	players.map(player => {
-    	const teamExists = result.find((item) => {return item.team == player.team})
-      if(!teamExists) { result.push({team: player.team, points: 0})  }
-    })
-    /* Queda temporariamente
-				[{
-          points: 0,
-          team: "red"
-        }, {
-          points: 0,
-          team: "blue"
-        }, {
-          points: 0,
-          team: "green"
-        }]
-	*/ 
-   players.map(player => {
-      if(player.isActive){
-          for(const item of result) {
-          /* Por cada equipo sumo los puntos  */
-            if(item.team === player.team){
-            	item.points += player.points
-            }
- 
-          }
-        
-       }
-     
-    })
-    return  result.sort(function (a, b) {
-      if (a.points < b.points) {
-        return 1;
+  const result = []
+  players.forEach(player => {
+    const teamExists = result.find((item) => { return item.team === player.team })
+    if (!teamExists) { result.push({ team: player.team, points: 0 }) }
+  })
+  /* Queda temporariamente [{points: 0,team: "red"},{points: 0,team: "blue"},{points: 0, team: "green"}] */
+  players.forEach(player => {
+    if (player.isActive) {
+      for (const item of result) {
+        /* Por cada equipo sumo los puntos  */
+        if (item.team === player.team) {
+          item.points += player.points
+        }
       }
-      if (a.points > b.points) {
-        return -1;
-      }
-      return 0;
-    });  
-}; 
+    }
+  })
+  return result.sort(function (a, b) {
+    if (a.points < b.points) {
+      return 1
+    }
+    if (a.points > b.points) {
+      return -1
+    }
+    return 0
+  })
+}
 
 console.log(calculateTeamPoints(players))
